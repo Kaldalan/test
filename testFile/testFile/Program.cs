302b6d1a047df34b2f048113b06f181fa16e13d5
Kaldalan <81563205+Kaldalan@users.noreply.github.com>
2022-10-11 09:44:46 +0300

    Add files via upload

---
+++ testFile/testFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
